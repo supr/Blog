d59dc05b4a677bc22882aed62348c11dae458de0
Prudhvi Krishna Surapaneni <me@prudhvi.net>
2011-06-08 18:07:25 -0500

    Add rudimentary view for view entries

--- blog/templates/index.mk
+++ blog/templates/index.mk
@@ -1,3 +1,12 @@
 <%inherit file="base.mk"/>
 
-<%block name="page_title">${title}</%block>
+<%block name="page_title">Welcome</%block>
+
+<%block name="page_content">
+%for entry in entries:
+	<h2>${entry['title'] | h,trim,unicode }</h2>
+	<p>Posted by ${entry['username'] | h} on ${entry['datetime'].strftime("%A, %d %B %Y at %I:%M %p")}</p>
+	<p>${entry['content'] | h,trim,unicode }</p>
+	<p>Tags:${",".join(entry['tags'])}</p>
+%endfor
+</%block>
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from pyramid.view import view_config
+from pymongo import DESCENDING
 
 @view_config( route_name = "home", renderer = "index.mk" )
 def home(request):
-    return { 'title':'Welcome'}
+    entries = request.db.Entries.find({},sort=[("datetime",DESCENDING)]).limit(4)
+    return { 'entries':entries }
 
 @view_config( route_name = "new", renderer = "new.mk", permission = "new" )
 def new_entry(request):
