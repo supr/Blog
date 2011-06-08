45b79e1a505392b2c9a04c26913ce1c83d550730
Prudhvi Krishna Surapaneni <me@prudhvi.net>
2011-06-08 18:14:02 -0500

    Support New Entries from the Web

--- blog/templates/new.mk
+++ blog/templates/new.mk
@@ -6,7 +6,9 @@
 		<h1> New Post </h1>
 		<form method="POST">
 			<input type="text" name="title"/>
-			<textarea name="contents"></textarea>
+			<textarea name="content"></textarea>
+			<input type="text" name="tags"/>
+			<input type="submit" value="Post" name="form.submitted"/>
 		</form>
 	</body>
 </html>
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from pyramid.view import view_config
 from pymongo import DESCENDING
+from pyramid.security import authenticated_userid
+from datetime import datetime
 
 @view_config( route_name = "home", renderer = "index.mk" )
 def home(request):
@@ -8,4 +10,24 @@ def home(request):
 
 @view_config( route_name = "new", renderer = "new.mk", permission = "new" )
 def new_entry(request):
+    message = ''
+    if 'form.submitted' in request.params:
+        try:
+            title = request.params['title']
+            content = request.params['content']
+            tags = [ x.strip() for x in request.params['tags'].split(",") ]
+            username = authenticated_userid(request)
+            entry = dict(
+                title = title,
+                content = content,
+                tags = tags,
+                username = username,
+                datetime = datetime.now(),
+                )
+            request.db.Entries.insert(entry)
+        except:
+            message = 'Invalid forum parameters'
+            return dict(
+                message = message
+                )
     return {}
